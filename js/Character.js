89f9108aa1a39a8a8f819817e9b3b8e5e6a08b10
Rosti <rostmus_mavr@mail.ru>
2022-07-10 12:01:58 +0300

    commit 2

--- js/Character.js
+++ js/Character.js
@@ -55,4 +55,21 @@ export default class Character {
             return new Error()
           }
       }
+      levelUp() {
+        if(this.health <= 0) {
+          return new error('нельзя повысить уровень умершего')
+        }
+        this.level += 1
+        this.attack += this.attack * 0.2
+        this.defence += this.defence * 0.2
+        this.health = 100
+
+      }
+
+      damage(points) {
+        if(this.health >= 0) {
+        this.health -=this.health * (1- this.defence/100)
+        }
+      }
   }
+  const sasa = new Character('sasas', 'Daemon')
